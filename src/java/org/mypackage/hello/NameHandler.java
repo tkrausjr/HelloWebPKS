8bfa1121f3a7371683f010537d2bd04e3cab499e
Thomas Kraus <kraust@vmware.com>
2018-07-30 11:18:01 -0400

    Working Added Dockerfile Build

--- src/java/org/mypackage/hello/NameHandler.java
+++ src/java/org/mypackage/hello/NameHandler.java
@@ -1,7 +1,7 @@
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
- * ADDED SOME STUFF
+ * ADDED SOME STUFF Monday 
  */
 
 package org.mypackage.hello;
